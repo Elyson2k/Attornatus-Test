84755740db79573eb10e3c057dc1324683244ecb
Elyson Vinicius <elysonvinicius@outlook.com>
2023-01-04 16:59:00 -0300

    feat: inserting data in application

--- src/main/java/com/project/apiperson/PersonApplication.java
+++ src/main/java/com/project/apiperson/PersonApplication.java
@@ -1,8 +1,8 @@
 package com.project.apiperson;
 
-import com.project.apiperson.entities.Address;
-import com.project.apiperson.entities.City;
-import com.project.apiperson.entities.Person;
+import com.project.apiperson.domain.entities.Address;
+import com.project.apiperson.domain.entities.City;
+import com.project.apiperson.domain.entities.Person;
 import com.project.apiperson.repository.AddressRepository;
 import com.project.apiperson.repository.CityRepository;
 import com.project.apiperson.repository.PersonRepository;
@@ -15,7 +15,7 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 @SpringBootApplication
-public class PersonApplication implements CommandLineRunner {
+public class PersonApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(PersonApplication.class, args);
@@ -40,4 +40,5 @@ public class PersonApplication implements CommandLineRunner {
 		cityRepository.saveAll(Arrays.asList(c1));
 		addressRepository.saveAll(Arrays.asList(ad1));
 	}
+
 }
