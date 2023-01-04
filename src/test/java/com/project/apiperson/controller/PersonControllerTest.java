8562c51779386c23ac198ce5e593e3f5beaa9423
Elyson Vinicius <elysonvinicius@outlook.com>
2023-01-04 16:50:44 -0300

    feat: creating test for personController

---
+++ src/test/java/com/project/apiperson/controller/PersonControllerTest.java
@@ -0,0 +1,130 @@
+package com.project.apiperson.controller;
+
+import com.project.apiperson.domain.dto.PersonAll;
+import com.project.apiperson.domain.dto.PersonDto;
+import com.project.apiperson.domain.dto.PersonPost;
+import com.project.apiperson.domain.dto.PersonPut;
+import com.project.apiperson.domain.entities.Address;
+import com.project.apiperson.domain.entities.City;
+import com.project.apiperson.domain.entities.Person;
+import com.project.apiperson.service.PersonService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.servlet.mvc.condition.RequestConditionHolder;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+
+class PersonControllerTest {
+
+    public static final int ID = 1;
+    public static final String NAME_PERSON = "Elyson";
+    public static final String STREET = "Rua";
+    public static final String CITY = "City";
+    public static final String EMAIL = "ElysonV@Outlook.com";
+    public static final String CPF = "00000000000";
+    public static final String ZIP_CODE = "ZipCode";
+    public static final String DATE = "25-02-2003";
+    public static final int NUMBER = 100;
+    public static final String ERROR_EMAIL_IS_ALREADY_BEING_USED = "Error: email is already being used.";
+    public static final String ERROR_ENTITY_NOT_FOUND = "Error: Entity not found.";
+    public static final String ERROR_NO_PERSON_FOUND = "Error: no person found.";
+
+    Person person;
+    Optional<Person> optionalPersonDto;
+    PersonAll personAll;
+    private PersonPost personPost;
+    private PersonDto personDto;
+
+    private PersonPut personPut;
+    private Address address;
+    @InjectMocks
+    private PersonController personController;
+    @Mock
+    private PersonService personService;
+
+
+    @BeforeEach
+    void setUp() throws ParseException {
+        MockitoAnnotations.openMocks(this);
+        startUser();
+        when(personService.findAllPerson()).thenReturn(List.of(personAll));
+        when(personService.findPersonByID(ID)).thenReturn(person);
+        when(personService.insertPerson(personPost)).thenReturn(person);
+        when(personService.chancePerson(personPut)).thenReturn(person);
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
+    }
+
+    @Test
+    void findPerson() {
+        var response = personController.findPerson(ID);
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(PersonDto.class, response.getBody().getClass());
+        assertEquals(ID, response.getBody().getId());
+        assertEquals(NAME_PERSON, response.getBody().getName());
+        assertEquals(EMAIL, response.getBody().getEmail());
+        assertEquals(CPF, response.getBody().getCpf());
+    }
+
+    @Test
+    void findListPersons() {
+        var response = personController.findListPersons();
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(PersonAll.class, response.getBody().get(0).getClass());
+        assertEquals(ID, response.getBody().get(0).getId());
+        assertEquals(NAME_PERSON, response.getBody().get(0).getName());
+        assertEquals(EMAIL, response.getBody().get(0).getEmail());
+        assertEquals(CPF, response.getBody().get(0).getCpf());
+    }
+
+    @Test
+    void updatePerson() {
+        var response = personController.updatePerson(ID, personPut).getStatusCode();
+        assertEquals(HttpStatus.OK, response);
+    }
+
+    @Test
+    void insertPerson() {
+        var response = personController.insertPerson(personPost);
+        assertNotNull(response);
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+    }
+
+    public void startUser() throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+
+        City city = new City(ID, CITY);
+        person = new Person(ID, NAME_PERSON, EMAIL, CPF , sdf.parse(DATE));
+        address = new Address(ID, STREET, ZIP_CODE, 100, person, city);
+        personAll = new PersonAll(ID, NAME_PERSON, EMAIL, CPF, sdf.parse(DATE));
+        personDto = new PersonDto(ID, NAME_PERSON, EMAIL, CPF , sdf.parse(DATE));
+        personPut = new PersonPut(ID,NAME_PERSON,EMAIL);
+        personPost = new PersonPost(NAME_PERSON, EMAIL, CPF, sdf.parse(DATE), STREET, ZIP_CODE, NUMBER, NUMBER);
+        optionalPersonDto = Optional.of(person);
+    }
+}
---
+++ src/test/java/com/project/apiperson/service/PersonServiceTest.java
@@ -0,0 +1,177 @@
+package com.project.apiperson.service;
+
+import com.project.apiperson.domain.dto.PersonAll;
+import com.project.apiperson.domain.dto.PersonDto;
+import com.project.apiperson.domain.dto.PersonPost;
+import com.project.apiperson.domain.dto.PersonPut;
+import com.project.apiperson.domain.entities.Address;
+import com.project.apiperson.domain.entities.City;
+import com.project.apiperson.domain.entities.Person;
+import com.project.apiperson.repository.AddressRepository;
+import com.project.apiperson.repository.PersonRepository;
+import com.project.apiperson.service.exceptions.CustomExceptions;
+import com.project.apiperson.service.exceptions.DataIntegrityViolationException;
+import com.project.apiperson.service.exceptions.ObjectNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+class PersonServiceTest {
+
+    public static final int ID = 1;
+    public static final String NAME_PERSON = "Elyson";
+    public static final String STREET = "Rua";
+    public static final String CITY = "City";
+    public static final String EMAIL = "ElysonV@Outlook.com";
+    public static final String CPF = "00000000000";
+    public static final String ZIP_CODE = "ZipCode";
+    public static final String DATE = "25-02-2003";
+    public static final int NUMBER = 100;
+    public static final String ERROR_EMAIL_IS_ALREADY_BEING_USED = "Error: email is already being used.";
+    public static final String ERROR_ENTITY_NOT_FOUND = "Error: Entity not found.";
+    public static final String ERROR_NO_PERSON_FOUND = "Error: no person found.";
+    public static final String DATE_FORMAT = "dd-MM-yyyy";
+
+    @InjectMocks
+    private PersonService personService;
+    @Mock
+    private PersonRepository personRepository;
+
+    @Mock
+    private AddressRepository addressRepository;
+    Person person;
+    Optional<Person> optionalPersonDto;
+    PersonAll personAll;
+    private PersonPost personPost;
+    private PersonDto personDto;
+    private Address address;
+    private PersonPut personPut;
+
+    @BeforeEach
+    void setUp() throws ParseException {
+        MockitoAnnotations.openMocks(this);
+        startUser();
+        when(personRepository.findAll()).thenReturn(List.of(person));
+        when(personRepository.findById(ID)).thenReturn(Optional.of(person));
+        when(personRepository.save(person)).thenReturn(person);
+    }
+
+
+    @Test
+    void findPersonByID() {
+        var response = personService.findPersonByID(ID);
+        assertNotNull(response);
+        assertEquals(NAME_PERSON, response.getName());
+        assertEquals(EMAIL, response.getEmail());
+        assertEquals(CPF, response.getCpf());
+    }
+
+    @Test
+    void findAllPerson() {
+        var response = personService.findAllPerson();
+        assertNotNull(response);
+        assertEquals(ID, response.get(0).getId());
+        assertEquals(NAME_PERSON, response.get(0).getName());
+        assertEquals(EMAIL, response.get(0).getEmail());
+        assertEquals(CPF, response.get(0).getCpf());
+    }
+
+
+    @Test
+    void insertPerson() {
+
+        var response = personService.insertPerson(personPost);
+        assertNotNull(response);
+        assertEquals(NAME_PERSON, response.getName());
+        assertEquals(EMAIL, response.getEmail());
+        assertEquals(CPF, response.getCpf());
+        assertEquals(STREET, response.getAddresses().get(0).getStreet());
+        assertEquals(ZIP_CODE, response.getAddresses().get(0).getZipCode());
+        assertEquals(NUMBER ,  response.getAddresses().get(0).getNumber());
+    }
+
+
+    @Test
+    void chancePerson() {
+        var response = personService.chancePerson(personPut);
+        assertNotNull(response);
+        assertEquals(NAME_PERSON, response.getName());
+        assertEquals(EMAIL, response.getEmail());
+        assertEquals(CPF, response.getCpf());
+    }
+
+
+    @Test
+    void testErrorDataIntegrity(){
+        when(personRepository.findByEmail(any())).thenThrow(new DataIntegrityViolationException(ERROR_EMAIL_IS_ALREADY_BEING_USED));
+
+        try{
+            personService.findEmail(personPost);
+        } catch (Exception ex){
+            assertEquals(DataIntegrityViolationException.class, ex.getClass());
+            assertEquals(ERROR_EMAIL_IS_ALREADY_BEING_USED, ex.getMessage());
+        }
+    }
+
+
+    @Test
+    void testErrorObjectNotFound(){
+        when(personRepository.findById(ID)).thenThrow(new ObjectNotFoundException(ERROR_ENTITY_NOT_FOUND));
+        try{
+            personService.findPersonByID(ID);
+        } catch (Exception ex){
+            assertEquals(ObjectNotFoundException.class, ex.getClass());
+            assertEquals(ERROR_ENTITY_NOT_FOUND, ex.getMessage());
+        }
+    }
+
+
+    @Test
+    void testErrorCustom(){
+        when(personRepository.findAll()).thenThrow(new CustomExceptions(ERROR_NO_PERSON_FOUND));
+        try{
+            personService.findAllPerson();
+        } catch (Exception ex){
+            assertEquals(CustomExceptions.class, ex.getClass());
+            assertEquals(ERROR_NO_PERSON_FOUND, ex.getMessage());
+        }
+    }
+
+    @Test
+    void testErrorCustomOuther(){
+        when(personRepository.findAll()).thenThrow(new CustomExceptions(ERROR_NO_PERSON_FOUND));
+        try{
+            var response = personService.findAllPerson();
+        } catch (Exception ex){
+            assertEquals(CustomExceptions.class, ex.getClass());
+            assertEquals(ERROR_NO_PERSON_FOUND, ex.getMessage());
+        }
+    }
+
+
+    public void startUser() throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT);
+
+        City city = new City(ID, CITY);
+        person = new Person(ID, NAME_PERSON, EMAIL, CPF , sdf.parse(DATE));
+        address = new Address(ID, STREET, ZIP_CODE, 100, person, city);
+        personAll = new PersonAll(ID, NAME_PERSON, EMAIL, CPF, sdf.parse(DATE));
+        personDto = new PersonDto(ID, NAME_PERSON, EMAIL, CPF , sdf.parse(DATE));
+        personPut = new PersonPut(ID, NAME_PERSON, EMAIL);
+        personPost = new PersonPost(NAME_PERSON, EMAIL, CPF, sdf.parse(DATE), STREET, ZIP_CODE, NUMBER, NUMBER);
+        optionalPersonDto = Optional.of(person);
+    }
+}
