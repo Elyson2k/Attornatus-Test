ab50a2ea68ba2c1e1e11c7a24694457d9268666d
Elyson Vinicius <elysonvinicius@outlook.com>
2023-01-04 22:44:50 -0300

    feat: creating tables with flyway

---
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -0,0 +1,27 @@
+CREATE TABLE tb_city(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name varchar(255)
+);
+
+CREATE TABLE tb_person(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name varchar(255),
+    email varchar(255),
+    cpf varchar(255),
+    date_of_birth datetime(6) NOT NULL
+);
+
+CREATE TABLE tb_address(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  street varchar(255),
+  zipcode varchar(255),
+  number INT,
+  person_id INT,
+  city_id INT,
+  FOREIGN KEY (person_id) REFERENCES tb_person(id)
+);
+
+
+
+
+
