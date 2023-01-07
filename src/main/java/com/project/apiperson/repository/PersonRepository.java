4e5a19abeb995d288caae5e9e481373842f8bff0
Elyson Vinicius <elysonvinicius@outlook.com>
2023-01-07 07:43:53 -0300

    feat: insert interface for personcontroller

--- src/main/java/com/project/apiperson/repository/PersonRepository.java
+++ src/main/java/com/project/apiperson/repository/PersonRepository.java
@@ -4,7 +4,10 @@ import com.project.apiperson.domain.entities.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 public interface PersonRepository extends JpaRepository<Person, Integer> {
     Optional<Person> findByEmail(String email);
+
+    Optional<Person> findByConfirmationToken(UUID fromString);
 }
