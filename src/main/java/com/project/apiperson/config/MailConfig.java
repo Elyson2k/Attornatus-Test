da0e47420592fc4a480cfb120b51be6de10737b7
Elyson Vinicius <elysonvinicius@outlook.com>
2023-01-07 07:41:08 -0300

    feat: inserting service email

---
+++ src/main/java/com/project/apiperson/config/MailConfig.java
@@ -0,0 +1,30 @@
+package com.project.apiperson.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("outlook.office365.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("elysonvinicius@outlook.com");
+        mailSender.setPassword("elisondota3");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "outlook.office365.com");
+        props.put("mail.smtp.port", "587");
+
+        return mailSender;
+    }
+}
---
+++ src/main/java/com/project/apiperson/service/EmailService.java
@@ -0,0 +1,35 @@
+package com.project.apiperson.service;
+
+import com.project.apiperson.domain.entities.Person;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailService {
+
+    private final JavaMailSender emailSender;
+
+    public EmailService(JavaMailSender emailSender) {
+        this.emailSender = emailSender;
+    }
+
+    public void sendSimpleMessage(Person person) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("elysonvinicius@outlook.com");
+        message.setTo(person.getEmail());
+        message.setSubject("[CHECK YOUR ACCOUT] Hello, welcome to Eloware!!!");
+        message.setText(buildEmailMessage(person));
+        emailSender.send(message);
+    }
+
+    private static String buildEmailMessage(Person personEntity) {
+        StringBuilder builder;
+        builder = new StringBuilder();
+        builder.append("Clique nesse link para confirmar sua conta: ");
+        builder.append("http://localhost:8080/api/v1/persons/confirmAccount?confirmationToken=");
+        builder.append(personEntity.getConfirmationToken().toString());
+        return builder.toString();
+    }
+
+}
